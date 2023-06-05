f974cd2dc185131f5af74876e3e5c0b17bdc1538
Tomas Otarola <76793058+Totootarola1@users.noreply.github.com>
2023-06-05 00:11:59 -0300

    Update reserva.js

--- reserva.js
+++ reserva.js
@@ -109,11 +109,6 @@ reserva.length = 0;
 btnComprar.addEventListener("click", comprarCarrito);
 // funcion para realizar compra
 function comprarCarrito() {
-      swal({
-        title: "Su reserva ha sido tomada!",
-        icon: "success",
-        button: "Aceptar!",
-      });
     reserva.length = 0;
     localStorage.setItem("productos-en-carrito", JSON.stringify(reserva));
     
